4074441d36d43c24aef2a7e88d3f7cc261621c8a
wafaaza1 <wafaaza2000@gmail.com>
2019-10-17 08:35:29 +0200

    koden

---
+++ php/include/db.php
@@ -0,0 +1,17 @@
+<?php
+include 'dbinfo.php';
+
+try {
+    $dbh = new PDO(
+        'mysql:host=localhost;charset=utf8mb4;dbname=' . $database . '',
+         $user,
+          $password
+    );
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+include 'views/index_layout.php';
+
+?>
