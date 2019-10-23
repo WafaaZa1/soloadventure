a2549da95145b7e533d8a9d41ab50ec25f66d697
wafaaza1 <wafaaza2000@gmail.com>
2019-10-23 09:36:28 +0200

    font, logo and background

--- php/views/index_layout.php
+++ php/views/index_layout.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $pageTitle; ?></title>
-    <link rel="stylesheet" href="css/style.css">
+    <title><?= $pageTitle; ?>soloadventure</title>
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
 <body>
-
     <div class="bg">
 	<nav class="navbar navbar-expand-md">
-  <a class="navbar-brand" href="#">Logo</a>
+  <a class="navbar-brand" href="#">
+    <img src="file:///C:/Users/wafaays.zamil.LEARNET/Desktop/Digitalt%20Skapande/logo4.svg">
+  </a>
   <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -24,31 +26,28 @@
       <li class="nav-item">
         <a class="nav-link" href="#">About</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-
-        
-      </li>
     </ul>
   </div>
 </nav>
 <main>
+<div class="mx-auto" style="width: 200px;">
         <p>
             <?= $story['body'] ?>
         </p>
-
         <ul>
             <?php foreach ($links as $link): ?>
-                <li>
+            <li> 
                 <a href="?id=<?= $link['target_id'] ?>">
+
                 <?= $link['description'] ?>
             <?php endforeach ?>
+            </div>
         </ul>
     </main>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-</div>
-</body>
+    </div>
+  </body>
 </html>
